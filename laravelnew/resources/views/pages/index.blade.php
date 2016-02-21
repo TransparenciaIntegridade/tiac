64ef4e8bca2415f14081b7013fcb0fb8d5991bf9
Celso Rodrigues <celso.rodrigues@transparencia.pt>
2016-02-21 17:20:15 +0000

    Adicionado projeto

---
+++ laravelnew/resources/views/pages/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 96px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <p>hello world</p>
+        <div class="title">Index page</div>
+    </div>
+</div>
+</body>
+</html>
